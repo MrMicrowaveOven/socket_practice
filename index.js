1f63e3c9055a2a657b3840a0bfd4df75e6808e23
Benjamin <benj@zagorski.com>
2016-06-09 11:23:32 -0700

    Display number of users connected, with disconnection glitch fixed

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+var NumConnected = 0;
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', function(socket){
-  // console.log('a user connected');
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  NumConnected++;
+  console.log('a user connected');
+  console.log(NumConnected + ' people connected');
+  socket.on('disconnect', function(){
+    NumConnected--;
+    console.log('user disconnected');
+    console.log(NumConnected + ' people connected');
+  });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
