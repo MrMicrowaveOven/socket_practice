b46ddf24636b2e16c55e6a3c06366225dac34d84
Benjamin <benj@zagorski.com>
2016-06-09 11:19:40 -0700

    SendingMessagesBetweenPagesAchieved

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ var io = require('socket.io')(http);
 var path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', { root: path.join(__dirname) });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  // console.log('a user connected');
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
